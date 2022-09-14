258b0c837d226ef08da8c7262116bd48bb721f0a
qinxiayu <qinxiayu@douban.com>
2022-09-14 16:50:08 +0800

    fix upyun exporter

--- httpRequest/httpRequest.go
+++ httpRequest/httpRequest.go
@@ -6,12 +6,27 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
+const domainListAddress = "https://api.upyun.com/buckets"
 const httpBandWidthAddress = "https://api.upyun.com/v2/statistics"
 const httpAccountHealthAddress = "https://api.upyun.com/flow/health_degree/detail"
 const httpBandWidthDetailAddress = "https://api.upyun.com/flow/common_data"
+
+type DomainList struct {
+	Domain  string `json:"domain"`
+	Status  string `json:"status"`
+}
+
+type BucketList struct {
+	Buckets []struct {
+		BucketId   int64        `json:"bucket_id"`
+		BucketName string       `json:"bucket_name"`
+		Domains    []DomainList `json:"domains"`
+	} `json:"buckets"`
+}
 
 type BandWidthList struct {
 	Data []struct {
@@ -23,31 +38,29 @@ type BandWidthList struct {
 	} `json:"data"`
 	Interval string `json:"interval"`
 }
-type BandWidthDetailList struct {
-	Code200   int64  `json:"_200"`
-	Code206   int64  `json:"_206"`
-	Code301   int64  `json:"_301"`
-	Code303   int64  `json:"_303"`
-	Code304   int64  `json:"_304"`
-	Code400   int64  `json:"_400"`
-	Code403   int64  `json:"_403"`
-	Code404   int64  `json:"_404"`
-	Code411   int64  `json:"_411"`
-	Code499   int64  `json:"_499"`
-	Code500   int64  `json:"_500"`
-	Code502   int64  `json:"_502"`
-	Code503   int64  `json:"_503"`
-	Code504   int64  `json:"_504"`
+
+type BandWidthDetail struct {
+	Code200   int  `json:"_200"`
+	Code206   int  `json:"_206"`
+	Code301   int  `json:"_301"`
+	Code303   int  `json:"_303"`
+	Code304   int  `json:"_304"`
+	Code400   int  `json:"_400"`
+	Code403   int  `json:"_403"`
+	Code404   int  `json:"_404"`
+	Code411   int  `json:"_411"`
+	Code499   int  `json:"_499"`
+	Code500   int  `json:"_500"`
+	Code502   int  `json:"_502"`
+	Code503   int  `json:"_503"`
+	Code504   int  `json:"_504"`
 	Bandwidth string `json:"bandwidth"`
-	Reqs      int64  `json:"reqs"`
+	Reqs      int  `json:"reqs"`
 }
-type AccountHealthList struct {
+
+type AccountHealth struct {
 	Result struct {
 		Code200 int64 `json:"_200"`
-		Code206 int64 `json:"_206"`
-		Code301 int64 `json:"_301"`
-		Code303 int64 `json:"_303"`
-		Code304 int64 `json:"_304"`
 		Code400 int64 `json:"_400"`
 		Code403 int64 `json:"_403"`
 		Code404 int64 `json:"_404"`
@@ -61,17 +74,15 @@ type AccountHealthList struct {
 	} `json:"result"`
 }
 
-func DoAccountHealthRequest(token string, rangeTime int64, delayTime int64) AccountHealthList {
-	endTime := time.Now().Add(-time.Minute * time.Duration(delayTime)).Format("2006-01-02:15:04:05")
-	startTime := time.Now().Add(-time.Minute * time.Duration(rangeTime)).Format("2006-01-02:15:04:05")
-	req, err := http.NewRequest("GET", httpAccountHealthAddress, nil)
+func DoDomainListRequest(token string) []string {
+	req, err := http.NewRequest("GET", domainListAddress, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	parm := make(url.Values)
-	parm.Add("start_time", startTime)
-	parm.Add("end_time", endTime)
-	req.URL.RawQuery = parm.Encode()
+	params := make(url.Values)
+	params.Add("business_type", "file")
+	params.Add("type", "ucdn")
+	req.URL.RawQuery = params.Encode()
 
 	req.Header.Set("Authorization", "Bearer "+token)
 
@@ -80,7 +91,51 @@ func DoAccountHealthRequest(token string, rangeTime int64, delayTime int64) Acco
 	if err != nil {
 		log.Fatal("请求失败", err)
 	}
-	var AccountHealth AccountHealthList
+	var (
+		bucketList BucketList
+		domainList []string
+	)
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	errJson := json.Unmarshal(body, &bucketList)
+	if errJson != nil {
+		log.Fatal(err)
+	}
+
+	for _, bucket := range bucketList.Buckets{
+		for _, domain := range bucket.Domains{
+			if strings.Contains(domain.Domain, "upaiyun") || strings.Contains(domain.Domain, "upcdn") {
+				continue
+			}
+			domainList = append(domainList, domain.Domain)
+		}
+	}
+	return domainList
+}
+
+func DoAccountHealthRequest(token string, rangeTime int64, delayTime int64) AccountHealth {
+	endTime := time.Now().Add(-time.Minute * time.Duration(delayTime)).Format("2006-01-02:15:04:05")
+	startTime := time.Now().Add(-time.Minute * time.Duration(rangeTime)).Format("2006-01-02:15:04:05")
+	req, err := http.NewRequest("GET", httpAccountHealthAddress, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	params := make(url.Values)
+	params.Add("start_time", startTime)
+	params.Add("end_time", endTime)
+	params.Add("flow_source", "cdn")
+	req.URL.RawQuery = params.Encode()
+
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		log.Fatal("请求失败", err)
+	}
+	var AccountHealth AccountHealth
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -92,9 +147,10 @@ func DoAccountHealthRequest(token string, rangeTime int64, delayTime int64) Acco
 	}
 	return AccountHealth
 }
+
 func DoHttpBandWidthRequest(domain string, token string, rangeTime int64, delayTime int64) BandWidthList {
-	endTime := time.Now().Add(-time.Minute * time.Duration(delayTime)).Format("2006-01-02:15:04:05")
-	startTime := time.Now().Add(-time.Minute * time.Duration(rangeTime)).Format("2006-01-02:15:04:05")
+	endTime := time.Now().Add(-time.Second * time.Duration(delayTime)).Format("2006-01-02:15:04:05")
+	startTime := time.Now().Add(-time.Second * time.Duration(rangeTime)).Format("2006-01-02:15:04:05")
 	req, err := http.NewRequest("GET", httpBandWidthAddress, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -103,7 +159,7 @@ func DoHttpBandWidthRequest(domain string, token string, rangeTime int64, delayT
 	parm.Add("start_time", startTime)
 	parm.Add("end_time", endTime)
 	parm.Add("flow_type", "cdn")
-	parm.Add("flow_source", "cdn")
+	parm.Add("flow_source", "backsource")
 	parm.Add("domain", domain)
 	req.URL.RawQuery = parm.Encode()
 	req.Header.Set("Authorization", "Bearer "+token)
@@ -124,9 +180,9 @@ func DoHttpBandWidthRequest(domain string, token string, rangeTime int64, delayT
 	return BandWidth
 }
 
-func DoHttpBandWidthResourceRequest(domain string, token string, rangeTime int64, delayTime int64) map[string][]BandWidthDetailList {
-	endTime := time.Now().Add(-time.Minute * time.Duration(delayTime)).Format("2006-01-02:15:04:05")
-	startTime := time.Now().Add(-time.Minute * time.Duration(rangeTime)).Format("2006-01-02:15:04:05")
+func DoHttpBandWidthResourceRequest(domain string, token string, rangeTime int64, delayTime int64) map[string][]BandWidthDetail {
+	endTime := time.Now().Add(-time.Second * time.Duration(delayTime)).Format("2006-01-02:15:04:05")
+	startTime := time.Now().Add(-time.Second * time.Duration(rangeTime)).Format("2006-01-02:15:04:05")
 	req, err := http.NewRequest("GET", httpBandWidthDetailAddress, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -151,7 +207,7 @@ func DoHttpBandWidthResourceRequest(domain string, token string, rangeTime int64
 	if err != nil {
 		log.Fatal(err)
 	}
-	var bandWidthDetail map[string][]BandWidthDetailList
+	var bandWidthDetail map[string][]BandWidthDetail
 	errJson := json.Unmarshal(body, &bandWidthDetail)
 	if errJson != nil {
 		log.Fatal(err)
--- main.go
+++ main.go
@@ -11,25 +11,26 @@ import (
 	"strconv"
 	"time"
 	"upyun-test/exporter"
+	"upyun-test/httpRequest"
 )
 
 var domainList []string
 
-func FetchDomainList() {
-
-	domainList = []string{"img1.doubanio.com"}
+func FetchDomainList(token string) {
+	domainList = httpRequest.DoDomainListRequest(token)
+	//domainList = []string{"img7.doubanio.com"}
 
 }
 
 func main() {
+	bucketToken := flag.String("bucket_token", os.Getenv("UpYun_Bucket_Token"), "upYun bucket token")
 	token := flag.String("token", os.Getenv("UpYun_Token"), "upYun token")
 	host := flag.String("host", "0.0.0.0", "服务监听地址")
 	port := flag.Int("port", 9300, "服务监听端口")
-	delayTime := flag.Int64("delayTime", 60, "时间偏移量, 结束时间=now-delay_seconds")
-	rangeTime := flag.Int64("rangeTime", 3000, "选取时间范围, 开始时间=now-range_seconds, 结束时间=now")
+	delayTime := flag.Int64("delayTime", 300, "时间偏移量, 结束时间=now-delay_seconds")
+	rangeTime := flag.Int64("rangeTime", 1800, "选取时间范围, 开始时间=now-range_seconds, 结束时间=now")
 	tickerTime := flag.Int("tickerTime", 10,  "刷新域名列表间隔时间")
-	flag.Parse()
-	FetchDomainList()
+	metricsPath := flag.String("metricsPath", "/metrics", "默认的metrics路径")
 	ticker := time.NewTicker(time.Duration(*tickerTime) * time.Second)
 	done := make(chan bool)
 	go func() {
@@ -38,7 +39,7 @@ func main() {
 			case <-done:
 				return
 			case <-ticker.C:
-				FetchDomainList()
+				FetchDomainList(*bucketToken)
 			}
 		}
 	}()
@@ -48,8 +49,18 @@ func main() {
 	listenAddress := net.JoinHostPort(*host, strconv.Itoa(*port))
 	log.Println(listenAddress)
 	log.Println("Running on", listenAddress)
-	http.Handle("/metrics", promhttp.Handler()) //注册
-	log.Fatal(http.ListenAndServe(listenAddress, nil))
+	http.Handle(*metricsPath, promhttp.Handler()) //注册
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte(`<html>
+           <head><title>UPYUN CDN Exporter</title></head>
+           <body>
+           <h1>Upyun cdn exporter</h1>
+           <p><a href='` + *metricsPath + `'>Metrics</a></p>
+           </body>
+           </html>`))
+	})
+
+	log.Fatal(http.ListenAndServe(listenAddress, nil))
 }
 
